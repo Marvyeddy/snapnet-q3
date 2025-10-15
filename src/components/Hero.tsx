4807efe436c9e5b0dfdb1d739159bb1ea7a4747b
Marvyeddy <anyatonwumarvelous32@gmail.com>
2025-10-15 12:58:11 +0100

    completed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <h1 className="text-[32px] md:text-[40px] lg:text-[48px] font-bold text-center lg:text-left leading-tight md:leading-tight">
             Mutual Investment
           </h1>
-          <p className="w-full max-w-[449px] text-[#A9A9A9] mt-[18px] md:mt-[28px] mb-[32px] md:mb-[44px] text-center lg:text-left mx-auto lg:mx-0 text-[16px] md:text-[18px] lg:text-[20px] leading-relaxed px-1 md:px-0">
+          <p className="w-full max-w-[449px] text-paragraph  mt-[18px] md:mt-[28px] mb-[32px] md:mb-[44px] text-center lg:text-left mx-auto lg:mx-0 text-[16px] md:text-[18px] lg:text-[20px] leading-relaxed px-1 md:px-0">
             At Finance we care about your future. We help you invest the way you
             want. So you can relax, have fun and let your fund grow.
           </p>
@@ -18,7 +18,7 @@ const Hero = () => {
             <button className="w-full max-w-[241px] px-4 py-2 cursor-pointer text-[18px] md:text-[20px] lg:text-[24px] text-white bg-[#27AE60] rounded-full mx-auto lg:mx-0 transition hover:brightness-110">
               Learn more
             </button>
-            <div className="h-1 mx-auto bg-[#27AE60] w-[40px] md:w-[55px] lg:w-[69.5px] rounded mt-2"></div>
+            <div className="h-1 mx-auto bg-btn  w-[40px] md:w-[55px] lg:w-[69.5px] rounded mt-2"></div>
           </div>
         </div>
 
@@ -27,12 +27,12 @@ const Hero = () => {
             src={HeroImg}
             alt="hero"
             loading="lazy"
-            className="object-contain w-full max-w-[250px] md:max-w-[320px] lg:max-w-[400px] transition-all"
+            className="object-contain "
           />
         </div>
       </div>
 
-      <div className="w-fit mx-auto mt-16  ">
+      <div className="w-fit mx-auto mt-16 ">
         <button className="cursor-pointer">
           <img src={Vector} alt="down-button" loading="lazy" />
         </button>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Twitter from "../assets/twitter.svg";
 import Facebook from "../assets/facebook.svg";
 import Medium from "../assets/medium.svg";
-import Logo from "../assets/Group 3513.svg";
+import Logo from "../assets/Group 3511.svg";
 import Menu from "../assets/align-text-left.svg";
 
 const links = [Twitter, Medium, Facebook];
@@ -9,7 +9,7 @@ const links = [Twitter, Medium, Facebook];
 const Navbar = () => {
   return (
     <nav className="max-w-[1172px] mt-[58px] max-md:mt-[30px] flex items-center justify-between px-6 mx-auto">
-      <ul className="gap-[26px] flex items-center">
+      <ul className="gap-[26px] flex items-center max-md:hidden">
         {links.map((link, idx) => (
           <li key={idx}>
             <a href="">
@@ -19,8 +19,9 @@ const Navbar = () => {
         ))}
       </ul>
 
-      <div className="max-md:hidden">
+      <div className="flex items-center gap-2">
         <img src={Logo} alt="logo" loading="lazy" />
+        <h1 className="text-[26px] text-foreground">Finanace</h1>
       </div>
 
       <button className="cursor-pointer">
